d0b67182df319e0850a93e05e73aba32b1506d91
William Ryuputra <williamryuputra@yahoo.co.id>
2023-03-14 11:32:17 -0400

    Adds Neccessary Components

--- playlist-app/src/Components/App/App.js
+++ playlist-app/src/Components/App/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from '../SearchBar/SearchBar.js' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return (
+      // Outer Element
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+  
+          <div className="App-playlist">
+            {/* <SearchResults />
+            <Playlist /> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+
 export default App;
+
